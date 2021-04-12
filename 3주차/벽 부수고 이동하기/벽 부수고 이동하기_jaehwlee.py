be31703d426cdc939ef5f760846b5aa744d3e21f
Jaehwan Lee <jaehwlee@gmail.com>
2021-04-12 10:44:19 +0900

    Update 벽 부수고 이동하기_jaehwlee.py

--- 3주차/벽 부수고 이동하기/벽 부수고 이동하기_jaehwlee.py
+++ 3주차/벽 부수고 이동하기/벽 부수고 이동하기_jaehwlee.py
@@ -23,12 +23,11 @@ def bfs(x, y, z):
       ny = now_y + dy[i]
       if 0 <= nx < n and 0 <= ny < m and visited[nx][ny][now_z]==0:
         # 다음 맵이 이동할 수 있는 곳일 때
-        if wall_map[nx][ny]==0:
-          visited[nx][ny][now_z] = visited[now_x][now_y][now_z] + 1
+        if wall_map[nx][ny]==0:          visited[nx][ny][now_z] = visited[now_x][now_y][now_z] + 1
           q.append((nx, ny, now_z))
         # 다음 맵이 벽인데 아직 벽을 안 부쉈을 때
         elif now_z == 0:
-          visited[nx][ny][1] = visited[now_x][now_y][now_z] + 1
+          visited[nx][ny][1] = visited[now_x][now_y][0] + 1
           q.append((nx, ny, 1))
   return -1
 
