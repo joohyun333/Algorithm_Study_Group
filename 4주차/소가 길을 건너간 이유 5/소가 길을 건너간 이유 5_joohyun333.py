df032918b2b8b11ce7a04ede2fe61665129b2502
joohyun333 <joohyun333@naver.com>
2021-04-19 11:55:40 +0900

    Update 소가 길을 건너간 이유 5_joohyun333.py

--- 4주차/소가 길을 건너간 이유 5/소가 길을 건너간 이유 5_joohyun333.py
+++ 4주차/소가 길을 건너간 이유 5/소가 길을 건너간 이유 5_joohyun333.py
@@ -12,11 +12,10 @@ count = 0
 for i in range(1, K+1):
     if not board[i]:
         count += 1
-
 DP[K] = count
 result = count
 for i in range(K+1, N+1):
-    before = DP[i - 1]
+    before = DP[i - 1] # 망가진 신호등 수
     if not board[i - K]:
         before -= 1
     if not board[i]:
